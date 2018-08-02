d71839cf7be9b0028dff6bb14943488d45d10004
Jan Szumiec <jan.szumiec@gmail.com>
2018-08-02 21:34:01 +0100

    Ignore case.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/search')
 def search():
   with open("data/pl.txt") as f:
-    results = sorted(f.read().splitlines(), key=lambda p: distance(p, request.args.get('s')))
+    results = sorted(f.read().splitlines(), key=lambda p: distance(p.lower(), request.args.get('s')))
     return jsonify(results[:100])
 
 if __name__ == '__main__':
