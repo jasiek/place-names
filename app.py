735e18c8f5a0e33bf6edad0cf56ce70b1d537538
Jan Szumiec <jan.szumiec@gmail.com>
2018-08-02 21:39:55 +0100

    Serve some static assets.

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from Levenshtein import distance
 app = Flask(__name__)
+
 
 @app.route('/search')
 def search():
   with open("data/pl.txt") as f:
     results = sorted(f.read().splitlines(), key=lambda p: distance(p.lower(), request.args.get('s')))
     return jsonify(results[:100])
+
+@app.route('/<path:path>')
+def serve_static(path):
+  return send_from_directory('public', path)
+
+@app.route('/')
+def serve_index():
+  return serve_static('index.html')
 
 if __name__ == '__main__':
   app.run(debug=True, use_reloader=True)
