1297ef40bcab03d6821e5584ad519a8e5c50af67
Jan Szumiec <jan.szumiec@gmail.com>
2018-08-03 10:55:27 +0100

    Preload place names.

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask, request, jsonify, send_from_directory
 from Levenshtein import distance
 app = Flask(__name__)
 
+places = []
+with open("data/pl.txt") as f:
+  places = f.read().splitlines()
 
 @app.route('/search')
 def search():
-  with open("data/pl.txt") as f:
-    results = sorted(f.read().splitlines(), key=lambda p: distance(p.lower(), request.args.get('s')))
-    return jsonify(results[:100])
+  results = sorted(places, key=lambda p: distance(p.lower(), request.args.get('s')))
+  return jsonify(results[:100])
 
 @app.route('/<path:path>')
 def serve_static(path):
